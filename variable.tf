df6d5199e71c456ca6d0a5ae24e47000c55cc0f5
rallyuthappa <rally123@gmail.com>
2018-04-25 09:53:46 -0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "resource_group" {}
+variable "location" {}
+variable "vmsize" {}
+variable "capacity" {}
+variable "vmssname" {}
+variable "adminusername" {}
+variable "adminpassword" {}
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
